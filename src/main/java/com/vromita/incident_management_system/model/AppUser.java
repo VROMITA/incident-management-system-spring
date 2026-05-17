f3eb7f8402d8df1a088cbe9b72422286e1e35e09
Valerio <114232053+VROMITA@users.noreply.github.com>
2026-05-17 15:37:33 +0200

    Implement AppUserService and AppUser UserDetails interface

--- src/main/java/com/vromita/incident_management_system/model/AppUser.java
+++ src/main/java/com/vromita/incident_management_system/model/AppUser.java
@@ -1,12 +1,17 @@
 package com.vromita.incident_management_system.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity
 @Table(name = "app_users")
-public class AppUser {
+public class AppUser implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,8 +42,35 @@ public class AppUser {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + role.name())
+        );
     }
 
     public String getPassword() {
--- src/main/java/com/vromita/incident_management_system/service/AppUserService.java
+++ src/main/java/com/vromita/incident_management_system/service/AppUserService.java
@@ -1,4 +1,26 @@
 package com.vromita.incident_management_system.service;
 
-public class AppUserService {
+import com.vromita.incident_management_system.repository.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username){
+
+        return appUserRepository.findByUsername(username).orElseThrow(() ->
+        new UsernameNotFoundException("User not found with username: " + username));
+    }
+
+
 }
