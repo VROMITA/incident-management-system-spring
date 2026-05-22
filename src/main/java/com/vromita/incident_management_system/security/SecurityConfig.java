59e2927ef3b8b1150e947360487acaa69fe79750
Valerio <114232053+VROMITA@users.noreply.github.com>
2026-05-22 20:17:10 +0200

    SecurityConfig implemented with the following methods: passwordEncoder - securityFilterChain - authenticationManagerBean

---
+++ src/main/java/com/vromita/incident_management_system/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.vromita.incident_management_system.security;
+
+import com.vromita.incident_management_system.service.AppUserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AppUserService appUserService;
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(AppUserService appUserService, JwtFilter jwtFilter) {
+        this.appUserService = appUserService;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/auth/**").permitAll() // public path
+                        .anyRequest().authenticated() // others needs authentication
+
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration config ) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
