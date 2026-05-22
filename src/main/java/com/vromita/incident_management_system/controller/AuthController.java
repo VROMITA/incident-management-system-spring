3e30bfe3208c76b69aef845cea23ec261ef65072
Valerio <114232053+VROMITA@users.noreply.github.com>
2026-05-22 21:38:05 +0200

    AuthRequest DTO and AuthController implemented

---
+++ src/main/java/com/vromita/incident_management_system/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.vromita.incident_management_system.controller;
+
+import com.vromita.incident_management_system.dto.AuthRequest;
+import com.vromita.incident_management_system.security.JwtUtil;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * REST controller for managing authentication
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    /**
+     * The method firstly authenticate the user then extract the credentials and create the token
+     * @param request
+     * @return token for the user
+     */
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid AuthRequest request) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        // Extract the authentified user
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return ResponseEntity.ok(jwtUtil.generateToken(userDetails));
+
+    }
+}
--- src/main/java/com/vromita/incident_management_system/security/SecurityConfig.java
+++ src/main/java/com/vromita/incident_management_system/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     }
 
     @Bean
+
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
