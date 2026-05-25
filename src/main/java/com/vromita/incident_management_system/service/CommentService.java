c24a0f88ae30c12370d7e25d92fab7853b33adb1
Valerio <114232053+VROMITA@users.noreply.github.com>
2026-05-25 23:11:40 +0200

    Add SLA reset on comment by assigned team

--- src/main/java/com/vromita/incident_management_system/service/CommentService.java
+++ src/main/java/com/vromita/incident_management_system/service/CommentService.java
@@ -3,13 +3,15 @@ package com.vromita.incident_management_system.service;
 import com.vromita.incident_management_system.dto.CommentRequest;
 import com.vromita.incident_management_system.exception.IncidentNotFoundException;
 import com.vromita.incident_management_system.model.Comment;
+import com.vromita.incident_management_system.model.Incident;
 import com.vromita.incident_management_system.repository.AppUserRepository;
 import com.vromita.incident_management_system.repository.CommentRepository;
 import com.vromita.incident_management_system.repository.IncidentRepository;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class CommentService{
@@ -32,10 +34,21 @@ public class CommentService{
         Comment comment = new Comment();
 
         comment.setText(commentRequest.getText());
-        comment.setIncident(incidentRepository.findById(incidentId).orElseThrow(() ->
-                new IncidentNotFoundException("Incident with id " + incidentId + " not found")));
+
+        Incident incident = incidentRepository.findById(incidentId).orElseThrow(() ->
+                new IncidentNotFoundException("Incident with id " + incidentId + " not found"));
+        comment.setIncident(incident);
         comment.setAuthor(appUserRepository.findByUsername(username).orElseThrow(() ->
                 new UsernameNotFoundException("Username " + username + " not found")));
+
+
+        if(comment.getAuthor().getRole().name().equals(incident.getAssignedTeam().name())){
+
+            incident.setSlaDeadline(LocalDateTime.now().plusHours(incident.getPriority().getSlaHours()));
+
+            incidentRepository.save(incident);
+
+        }
 
         return commentRepository.save(comment);
 
